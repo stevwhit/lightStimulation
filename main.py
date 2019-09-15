90dee7558698ee9bff4e76da0e03c8dca4362327
peterstaylor <peters.taylor@gmail.com>
2019-09-15 17:25:10 -0400

    adding ascii art and start up instructions to code

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # main code for light stimulatio project
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
-
-#       Well Plate Layout
-#
-#
-#   A 
+while(1):
+    print("Welcome to the Light Stimulation set up process!\n")
+    print("Below is a visual representation of the LED array.\n ")
+    print("Please select a column you would like to program.\n")
+    print("|-----|-----|-----|-----|-----|-----|-----|")
+    print("|  /  |  A  |  B  |  C  |  D  |  E  |  F  |")
+    print("|  1  | *** | *** | *** | *** | *** | *** |")
+    print("|  2  | *** | *** | *** | *** | *** | *** |")
+    print("|  3  | *** | *** | *** | *** | *** | *** |")
+    print("|  4  | *** | *** | *** | *** | *** | *** |")
+    print("|-----|-----|-----|-----|-----|-----|-----|")
+    column = input("Selection, A - F: ")
