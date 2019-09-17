1c3eb33d70bc3351bf3ab4df5c9e80604dbfa0de
peterstaylor <peters.taylor@gmail.com>
2019-09-17 08:34:18 -0400

    adding a few intiial functions + GPIO code

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 # main code for light stimulatio project
 
 # below ascii art indicates pinout
-# pins marked with xs are used in this code
+# pins marked with A-F indicate the column they control
 #              Pin 1 Pin2
 #           +3V3 [ ] [ ] +5V
 # SDA1 / GPIO  2 [ ] [ ] +5V
 # SCL1 / GPIO  3 [ ] [ ] GND
-#        GPIO  4 [ ] [x] GPIO 14 / TXD0
-#            GND [ ] [x] GPIO 15 / RXD0
-#        GPIO 17 [ ] [x] GPIO 18
+#        GPIO  4 [ ] [A] GPIO 14 / TXD0
+#            GND [ ] [B] GPIO 15 / RXD0
+#        GPIO 17 [ ] [C] GPIO 18
 #        GPIO 27 [ ] [ ] GND
-#        GPIO 22 [ ] [x] GPIO 23
-#           +3V3 [ ] [x] GPIO 24
+#        GPIO 22 [ ] [D] GPIO 23
+#           +3V3 [ ] [E] GPIO 24
 # MOSI / GPIO 10 [ ] [ ] GND
-# MISO / GPIO  9 [ ] [x] GPIO 25
+# MISO / GPIO  9 [ ] [F] GPIO 25
 # SCLK / GPIO 11 [ ] [ ] GPIO  8 / CE0#
 #            GND [ ] [ ] GPIO  7 / CE1#
 #ID_SD / GPIO  0 [ ] [ ] GPIO  1 / ID_SC
@@ -25,7 +25,27 @@
 #            GND [ ] [ ] GPIO 21 / SCLK
 #             Pin 39 Pin 40
 
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+
+# set the pin numbering scheme
+GPIO.setmode(BCM)
+#set all pins to output mode
+GPIO.setup(14, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+
+def columnSet(column):
+    #todo: write a function that takes user column input and
+    # sets up frequency and duty cycle
+    x = 0 #placeholder code
+
+def PWMstart():
+    # todo: write function that starts PWM for all desired columns
+    x = 0
+
 
 while(1):
     print("Welcome to the Light Stimulation set up process!\n")
